589d4dd444ab6ae3b3313580795eb673dd0ce489
nelsonlobo99 <nelsonlobo@protonmail.com>
2020-01-15 16:09:07 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+// This app is a stateful, it tracks the user's current choice.
+class BasicAppBarSample extends StatefulWidget {
+  @override
+  _BasicAppBarSampleState createState() => _BasicAppBarSampleState();
+}
+
+class _BasicAppBarSampleState extends State<BasicAppBarSample> {
+  Choice _selectedChoice = choices[0]; // The app's "state".
+
+  void _select(Choice choice) {
+    // Causes the app to rebuild with the new _selectedChoice.
+    setState(() {
+      _selectedChoice = choice;
+      if(choice.title == "Exit")
+        exit(0);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Basic AppBar'),
+          actions: <Widget>[
+            
+            PopupMenuButton<Choice>(
+              onSelected: _select,
+              itemBuilder: (BuildContext context) {
+                return choices.map((Choice choice) {
+                  return PopupMenuItem<Choice>(
+                    value: choice,
+                    child: Text(choice.title),
+                  );
+                }).toList();
+              },
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          //child: ChoiceCard(choice: _selectedChoice),
+        ),
+        floatingActionButton: Container(
+          child: FloatingActionButton(
+            backgroundColor: Colors.red,
+            onPressed: (){
+              
+            },
+            child: Icon(
+              Icons.add,
+              size: 50.0)
+            ),
+            width: 75,
+            height: 75,
+          
+        ),
+      ),
+    );
+  }
+}
+
+class Choice {
+  const Choice({this.title, this.icon});
+
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'Settings', icon: Icons.settings),
+  const Choice(title: 'Exit', icon: Icons.exit_to_app),
+];
+/*
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({Key key, this.choice}) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = Theme.of(context).textTheme.display1;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(choice.icon, size: 128.0, color: textStyle.color),
+            Text(choice.title, style: textStyle),
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
+void main() {
+  runApp(BasicAppBarSample());
+}
